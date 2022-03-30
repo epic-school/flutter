9cc38fd1c092e8268aa8ff10d8926e0579e92775
Alexey M. Pereverzev <boss.pahan@gmail.com>
2022-03-30 23:52:09 +0300

    Добавлено ДЗ по 4 уроку. Классы

--- dart/bin/lesson_04/hw_classes.dart
+++ dart/bin/lesson_04/hw_classes.dart
@@ -3,6 +3,33 @@
 // 3. Добавьте в класс метод, getter который красиво форматирует Student для печати полей firstName и lastName (должны возвращать с заглавной буквы)
 // 4. Переопределите в классе Student метод toString для печати класса
 
+class Student {
+  final String firstName;
+  final String lastName;
+
+  const Student({required this.firstName, required this.lastName});
+
+  Student.named({this.firstName = 'имя', this.lastName = 'рек'});
+
+  String get fullName => firstName.isEmpty
+      ? 'First name is empty'
+      : lastName.isEmpty
+          ? 'Last name is empty'
+          : 'Имя: ${_lowUp(firstName)} \nФамилия: ${_lowUp(lastName)}';
+
+  String _lowUp(String str) =>
+      str.toLowerCase().replaceRange(0, 1, str[0].toUpperCase());
+
+  @override
+  String toString() {
+    return this.fullName;
+  }
+}
+
 void main(List<String> arguments) {
-  
+  final student = Student(firstName: 'aleXEy', lastName: 'pereVerZev');
+  final student2 = Student.named();
+
+  print(student.fullName);
+  print(student2);
 }
