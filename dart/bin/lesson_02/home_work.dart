57decc1d19e52893159d54854feaa58a7ee25ecc
work <work@ngd0055468.gd.rt.ru>
2022-03-31 20:05:49 +0300

    home work lesson 02 by Evgan

--- dart/bin/lesson_02/home_work.dart
+++ dart/bin/lesson_02/home_work.dart
@@ -8,9 +8,18 @@
 /// IN: 3, ['день', 'дня', 'дней']
 /// OUT: '3 дня'
 
+String getNumWord(int count, List<String> variants) {
+  final int lastTwoSymbol = count % 100;
+  if (lastTwoSymbol > 10 && lastTwoSymbol < 20) return variants[2];
+  final int lastSymbol = count % 10;
+  if (lastSymbol > 1 && lastSymbol < 5) return variants[1];
+  if (lastSymbol == 1) return variants[0];
+  return variants[2];
+}
+
 String numeral(int count, List<String> variants) {
   // ваш код
-  return "";
+  return '${count.toString()} ${getNumWord(count, variants)}';
 }
 
 /// 2-я функция ago, принимает 1 аргумент
@@ -27,9 +36,42 @@ String numeral(int count, List<String> variants) {
 /// IN: DateTime.now().subtract(Duration(minutes: 10)
 /// OUT: '10 минут назад'
 
+const int secInMin = 60;
+const int secInHour = 3600;
+const int secInDay = 86400;
+const int secInWeek = 604800;
+const int secInMonth = 1814400; // 4 week
+const int secInYear = 31557600;
+
+String getAgoTimeWord(int difTime, int sampling, List<String> variants) {
+  final double count = difTime / sampling;
+  return '${numeral(count.floor(), variants)} назад';
+}
+
 String ago(DateTime date) {
   // Ваш код
-  return "";
+  final dateNow = DateTime.now();
+  final dif = dateNow.difference(date);
+  final int difSec = dif.inSeconds;
+  if (difSec >= secInYear) {
+    return getAgoTimeWord(difSec, secInYear, ['год', 'года', 'лет']);
+  }
+  if (difSec >= secInMonth) {
+    return getAgoTimeWord(difSec, secInMonth, ['месяц', 'месяца', 'месяцев']);
+  }
+  if (difSec >= secInWeek) {
+    return getAgoTimeWord(difSec, secInWeek, ['неделю', 'недели', 'недель']);
+  }
+  if (difSec >= secInDay) {
+    return getAgoTimeWord(difSec, secInDay, ['день', 'дня', 'дней']);
+  }
+  if (difSec >= secInHour) {
+    return getAgoTimeWord(difSec, secInHour, ['час', 'часа', 'часов']);
+  }
+  if (difSec >= secInMin) {
+    return getAgoTimeWord(difSec, secInMin, ['минуту', 'минуты', 'минут']);
+  }
+  return getAgoTimeWord(difSec, 1, ['секунду', 'секунды', 'секунд']);
 }
 
 void main(List<String> arguments) {
