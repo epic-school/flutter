d46c5a08361c6aeff5f294296ea9689ede7b294e
takahasi6661 <60356662+takahasi6661@users.noreply.github.com>
2022-04-03 21:26:46 +0300

    Update home_work.dart

--- dart/bin/lesson_05/home_work.dart
+++ dart/bin/lesson_05/home_work.dart
@@ -1,35 +1,29 @@
-///
-/// Необходимо создать консольное приложение, которое слушало бы поток stdin
-/// (stdin содержит метод listen) и отправляла бы полученные результаты в
-/// другой поток, который обрабатывал бы их и выводил результат вычисления,
-///
-/// Обработка должна происходить следующим образом:
-/// 1. Если число является квадратом для какого-либо целого числа: выводим
-/// сообщение о том, квадратом какого числа является введенное число (см. тестовые данные)
-///
-/// 2. Если число не является квадратом для какого-либо целого числа: выводим
-/// соответствующее сообщение
-///
-/// 3. Если введенное значение целым числом не является - поток не должен получить
-/// его, сообщение выводим на этапе обработки значения в слушателе stdin
-///
-///////////////////////////////////////////////////////////////////////////////
-/// Тестовые данные для проверки своего решения
-///
-/// 0
-/// [Event handler] Event is square of 0
-/// abc
-/// [Stdin] Input must be integer
-/// 23
-/// [Event handler] Event is not a square of any integer number
-/// 24
-/// [Event handler] Event is not a square of any integer number
-/// 121
-/// [Event handler] Event is square of 11
-/// 49
-/// [Event handler] Event is square of 7
-///
+import 'dart:async';
+
+import 'dart:io';
+
+import 'dart:math';
 
 void main() {
+  var streamController = StreamController();
+  var stream = streamController.stream.asBroadcastStream();
 
+  stdin.listen((event) {
+    final eventToString = String.fromCharCodes(event);
+    var maybeInt = int.tryParse(eventToString);
+
+    if(maybeInt != null && maybeInt >=0) {
+      streamController.add(maybeInt);
+    } else {
+      print("[Stdin] Input must be integer");
+    }
+  });
+  stream.listen((event) {
+    final sqrtEvent=sqrt(event);
+    if(sqrtEvent != sqrtEvent.roundToDouble()) {
+      print("[Event handler] Event is not a square of any integer number");
+    } else {
+      print("[Event handler] Event is square ${sqrtEvent.toInt()}");
+    }
+  });
 }
