916fc5a4554790fded999ca0b9c8e5286cf6672e
MrBastr <60104763+MrBastr@users.noreply.github.com>
2022-04-05 00:46:38 +0300

    Бастрыкин С.А. hw4

--- dart/bin/lesson_04/hw_classes.dart
+++ dart/bin/lesson_04/hw_classes.dart
@@ -2,7 +2,23 @@
 // 2. Добавьте в класс конструктор, инициализацию всех свойств.
 // 3. Добавьте в класс метод, getter который красиво форматирует Student для печати полей firstName и lastName (должны возвращать с заглавной буквы)
 // 4. Переопределите в классе Student метод toString для печати класса
-
+import 'package:strings/strings.dart';
 void main(List<String> arguments) {
-  
+  Student student = Student();
+  student.firstName = 'John';
+  student.lastName = 'Wick';
+  print(student.toString());
+}
+
+class Student{
+  final String firstName;
+  final String lastName;
+  const ToyotaCars(this.firstName, this.lastName);
+  String get AllName(String name) => (capitalize(name));
+
+  @override
+  String toString() {
+    return 'Student{firstName: ${AllName(firstName)}, lastName: ${AllName(lastName)}';
+  }
+
 }
--- dart/bin/lesson_04/hw_solid.dart
+++ dart/bin/lesson_04/hw_solid.dart
@@ -6,4 +6,93 @@
 /// 
 /// Не запрещено брать примеры из вашего опыта или каких-либо источников, главное написать на Dart
 /// и раскрыть суть правил SOLID
-/// 
+///
+
+/// S: Single Responsibility Principle (Принцип единственной ответственности).
+// Calculations and logic
+abstract class Shape {
+  double area();
+}
+
+class Square extends Shape {}
+class Circle extends Shape {}
+class Rectangle extends Shape {}
+
+// UI painting
+class ShapePainter {
+  void paintSquare(Canvas c) {
+    /* ... */ }
+  void paintCircle(Canvas c) {
+    /* ... */ }
+  void paintTriangle(Canvas c) {
+    /* ... */ }
+}
+
+// Networking
+class ShapesOnline {
+  String wikiArticle(String figure) {
+    /* ... */ }
+  void _cacheElements(String text) {
+    /* ... */ }
+}
+///O: Open-Closed Principle (Принцип открытости-закрытости).
+abstract class Area {
+  const Area();
+
+  double computeArea();
+}
+
+class Rectangle extends Area {
+  final double width;
+  final double height;
+  const Rectangle(this.width, this.height);
+
+  @override
+  double computeArea() => width * height;
+}
+
+class Circle extends Area {
+  final double radius;
+  const Circle(this.radius);
+
+  @override
+  double computeArea() => radius * radius * 3.1415;
+}
+
+class AreaCalculator {
+  double calculate(Area shape) => shape.computeArea();
+}
+///L: Liskov Substitution Principle (Принцип подстановки Барбары Лисков).
+void main() {
+  final Rectangle error = Square(3);
+
+  // Creating a square with various sides lengths... what??
+  error.width = 4;
+  error.height = 8;
+}
+
+///I: Interface Segregation Principle (Принцип разделения интерфейса).
+abstract class Worker {
+  void work();
+}
+
+abstract class Sleeper {
+  void sleep();
+}
+
+class Human implements Worker, Sleeper {
+  void work() => print("I do a lot of work");
+  void sleep() => print("I need 10 hours per night...");
+}
+
+class Robot implements Worker {
+  void work() => print("I always work");
+}
+
+///D: Dependency Inversion Principle (Принцип инверсии зависимостей).
+void main() {
+  const fm = FileManager();
+
+  fm.secureFile(const AlgoAES());
+  fm.secureFile(const AlgoRSA());
+}
