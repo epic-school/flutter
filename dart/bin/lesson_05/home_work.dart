bb28c11b0a2635cb053d65f23c34c526b795219e
Alexey M. Pereverzev <boss.pahan@gmail.com>
2022-04-05 14:56:11 +0300

    Добавлено ДЗ по 5 уроку. Streams

--- dart/bin/lesson_05/home_work.dart
+++ dart/bin/lesson_05/home_work.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
 ///
 /// Необходимо создать консольное приложение, которое слушало бы поток stdin
 /// (stdin содержит метод listen) и отправляла бы полученные результаты в
@@ -31,5 +35,25 @@
 ///
 
 void main() {
+  var streamController = StreamController();
+  var stream = streamController.stream;
 
+  stream.listen((event) {
+    var number = sqrt(event);
+    if (number.toInt() != number) {
+      print("[Event handler] Event is not a square of any integer number");
+    } else {
+      print("[Event handler] Event is square of ${number.toInt()}");
+    }
+  });
+
+  stdin.listen((event) {
+    var input = systemEncoding.decode(event);
+    var maybeInt = int.tryParse(input);
+    if (maybeInt != null && !maybeInt.isNegative) {
+      streamController.add(maybeInt);
+    } else {
+      print('[Stdin] Input must be positive integer');
+    }
+  });
 }
