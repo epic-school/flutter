7a658b268d74f638ee9f18388c46488755c8cfe8
chevic <victor.chinenov@gmail.com>
2022-04-12 00:14:43 +0300

    dz vizitka

--- flutter_base/lib/main.dart
+++ flutter_base/lib/main.dart
@@ -1,115 +1,150 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return const MaterialApp(
+      home: Scaffold(
+        body: Vizitka(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Vizitka extends StatelessWidget {
+  const Vizitka({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return const Center(
+      child: SizedBox(
+        width: double.infinity,
+        height: 250,
+        child: Card(
+          color: Color.fromARGB(1, 135, 135, 135),
+          child: soderzhanie(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class soderzhanie extends StatelessWidget {
+  const soderzhanie({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              const CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1Rkbhi-oMFkToNuShgW1E6swqaKTM5SRkZCeTgDn6uOyic'),
+                radius: 50,
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Степан Иванов',
+                          style: TextStyle(fontSize: 26)),
+                      const Text('flutter-разработчик'),
+                      const Divider(),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: const [
+                              Icon(Icons.contacts,
+                                  size: 12,
+                                  color: Color.fromARGB(255, 54, 82, 244)),
+                              Text(' Github ', style: TextStyle(fontSize: 14)),
+                              Text('@StepanIvanov'),
+                            ],
+                          ),
+                          Row(
+                            children: const [
+                              Icon(Icons.contact_mail,
+                                  size: 12,
+                                  color: Color.fromARGB(255, 54, 82, 244)),
+                              Text(' Telegran ',
+                                  style: TextStyle(fontSize: 14)),
+                              Text('@Stepan12'),
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Divider(
+          height: 10,
+          thickness: 0.5,
+          indent: 20,
+          endIndent: 20,
+          color: Color.fromARGB(255, 37, 37, 37),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              tabloid(
+                  nameiNg: 'зарплата',
+                  namiN2: 'от 200000 ',
+                  icon: Icons.money_rounded),
+              tabloid(
+                  nameiNg: 'опыт работы',
+                  namiN2: '2 года',
+                  icon: Icons.money_rounded),
+              tabloid(
+                  nameiNg: 'релокация',
+                  namiN2: 'Чебаркуль',
+                  icon: Icons.money_rounded),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class tabloid extends StatelessWidget {
+  final String nameiNg;
+  final String namiN2;
+  final IconData icon;
+  const tabloid({
+    Key? key,
+    required this.nameiNg,
+    required this.namiN2,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Icon(icon, size: 40, color: Color.fromARGB(255, 91, 91, 91)),
+          Text(nameiNg, style: TextStyle(fontSize: 16)),
+          Text('$namiN2', style: TextStyle(fontSize: 12)),
+        ],
+      ),
     );
   }
 }
