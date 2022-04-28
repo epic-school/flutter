d33e61f3193b33d445c7bc5a68265fb3de0d9b0a
takahasi6661 <60356662+takahasi6661@users.noreply.github.com>
2022-04-28 16:58:48 +0300

    hw-network
    hw-network

--- flutter_base/lib/network/api_service.dart
+++ flutter_base/lib/network/api_service.dart
@@ -3,12 +3,14 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter_base/network/models/api_model.dart';
 import 'package:flutter_base/network/models/post_model.dart';
+import 'package:flutter_base/network/models/comment_model.dart';
 import 'package:http/http.dart' as http;
 
 const _baseUrl = 'https://jsonplaceholder.typicode.com';
 
 abstract class ApiService {
   Future<ApiModel<List<PostModel>, Failure>> getPosts();
+  Future<ApiModel<List<CommentModel>, Failure>> getComments(int postId);
 }
 
 class ApiServiceImpl implements ApiService {
@@ -38,6 +40,28 @@ class ApiServiceImpl implements ApiService {
       return ApiModel.error(Failure());
     }
   }
+
+  @override
+  Future<ApiModel<List<CommentModel>, Failure>> getComments( int postId) async {
+    try {
+      final url = Uri.parse(_baseUrl + '/comments?postId=$postId');
+      final response = await http.get(url);
+      if (response.statusCode != 200) {
+        ApiModel.error(Failure());
+      }
+      await Future.delayed(const Duration(seconds: 1));
+      final now = DateTime.now();
+      final List<dynamic> json = jsonDecode(response.body);
+      final models = json.map((e) => CommentModel.fromJson(e));
+
+      print(DateTime.now().difference(now).inMilliseconds);
+
+      return ApiModel.success(models.toList());
+    } catch (e) {
+      return ApiModel.error(Failure());
+    }
+  }
+
 }
 
 class ApiServiceDio implements ApiService {
@@ -63,4 +87,32 @@ class ApiServiceDio implements ApiService {
       return ApiModel.error(Failure());
     }
   }
+
+  @override
+  Future<ApiModel<List<CommentModel>, Failure>> getComments( int postId) async {
+    try {
+      final response = await dio.get('/comments?postId=$postId');
+
+      final list = response.data as List<dynamic>;
+
+      final models = list.map((e) => CommentModel.fromJson(e));
+
+      // final response1 = await dio.get('/posts');
+      //
+      // final list1 = response1.data as List<dynamic>;
+      //
+      // final modelsPost = list1.map((e) => PostModel.fromJson(e));
+      // final post = modelsPost.toList()[postId];
+      // dynamic readyModels=models.toList();
+      // print(post.title);
+      // final l=readyModels.insert(0, post);
+      // return ApiModel.success(l);
+
+      return ApiModel.success(models.toList());
+
+     } catch (e) {
+      return ApiModel.error(Failure());
+    }
+  }
+
 }
--- flutter_base/lib/network/main.dart
+++ flutter_base/lib/network/main.dart
@@ -2,19 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_base/network/api_service.dart';
 import 'package:flutter_base/network/enums.dart';
 import 'package:flutter_base/network/models/post_model.dart';
+import 'package:flutter_base/network/models/comment_model.dart';
+
+
 
 void main() {
   runApp(const App());
 }
+final _key = GlobalKey<NavigatorState>();
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      navigatorKey: _key,
       debugShowCheckedModeBanner: false,
-      home: PostsScreen(),
+      home: const PostsScreen(),
     );
   }
 }
@@ -92,6 +97,13 @@ class _PostsView extends StatelessWidget {
           itemBuilder: (context, i) => ListTile(
             title: Text(posts[i].title),
             subtitle: Text(posts[i].body),
+            onTap: (){
+              _key.currentState!.push(
+              MaterialPageRoute(
+                builder: (context) =>  CommentsScreen(postId: posts[i].id, postTitle:posts[i].title,postBody:posts[i].body),
+              ),
+              );
+            },
           ),
         );
       case ContentState.loading:
@@ -101,6 +113,149 @@ class _PostsView extends StatelessWidget {
       case ContentState.empty:
         return const Center(
           child: Text('Пустой список постов'),
+        );
+      case ContentState.failure:
+        return const Center(
+          child: Text(
+            'Ууупс, что-то пошло не так',
+            style: TextStyle(color: Colors.red),
+          ),
+        );
+      default:
+        return const Center(
+          child: Text('Данные не загружены'),
+        );
+    }
+  }
+}
+
+
+class CommentsScreen extends StatefulWidget {
+  final int postId;
+  final String postTitle;
+  final String postBody;
+  const CommentsScreen({Key? key, required this.postId,required this.postTitle,required this.postBody}) : super(key: key);
+
+  @override
+  State<CommentsScreen> createState() => _CommentsScreenState();
+}
+
+class _CommentsScreenState extends State<CommentsScreen> {
+  final ApiService service = ApiServiceDio.instance;
+  var state = ContentState.initial;
+  final comments = <CommentModel>[];
+  var postBody='';
+  var postTitle='';
+
+  Future<void> load() async {
+    setState(() {
+      state = ContentState.loading;
+    });
+    final response = await service.getComments(widget.postId);
+    postBody=widget.postBody;
+    postTitle=widget.postTitle;
+    if (response.hasError) {
+      setState(() {
+        state = ContentState.failure;
+        comments.clear();
+      });
+    } else {
+      setState(() {
+        state = response.result!.isNotEmpty
+            ? ContentState.success
+            : ContentState.empty;
+        comments
+          ..clear()
+          ..addAll(response.result!);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    load();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Post with comments'),
+      ),
+      body: _CommentsView(
+        state: state,
+        comments: comments,
+        postBody: postBody,
+        postTitle: postTitle,
+      ),
+    );
+  }
+}
+
+class _CommentsView extends StatelessWidget {
+  final ContentState state;
+  final List<CommentModel> comments;
+  final String postBody;
+  final String postTitle;
+
+  const _CommentsView({
+    Key? key,
+    required this.state,
+    this.comments = const [],
+    required this.postBody,
+    required this.postTitle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    switch (state) {
+      case ContentState.success:
+        return Padding(padding: const EdgeInsets.all(16.0),
+        child:
+        SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(postTitle.toUpperCase(),
+                style: const TextStyle(
+                  color: Colors.pink,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                ),),
+              Text(postBody,
+                style: const TextStyle(
+                  color: Colors.pinkAccent,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(top:16.0),
+              child:
+              Text('Комментарии:',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                ),),),
+              ...comments.map(
+                  (e)=>ListTile(
+              title: Text(e.name),
+                    subtitle: Text(e.body),
+            ),
+            )
+                .toList(),
+            ],
+        )
+    ),
+    );
+      case ContentState.loading:
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      case ContentState.empty:
+        return const Center(
+          child: Text('Пустой список комментариев'),
         );
       case ContentState.failure:
         return const Center(
